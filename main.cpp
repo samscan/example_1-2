c133189564cdaf58fe979f8a5ea43989604e7541
samscan <scanlons@union.edu>
2024-01-23 00:44:58 +0000

    gooder

--- main.cpp
+++ main.cpp
@@ -1,22 +1,51 @@
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+AnalogIn potentiometer(A0);
+
+float potentiometerReading = 0.0;
+
+void uartTask();
+float potentiometerAngle(float potentiometerReading); 
+
 int main()
 {
-   DigitalIn gasDetector(D2);
-   DigitalIn overTempDetector(D3);
+    while (true) {
+        uartTask();
+    }
+}
 
-   DigitalOut alarmLed(LED3);
-   
-   gasDetector.mode(PullDown);   
-   overTempDetector.mode(PullDown);
+void uartTask()
+{
+    char receivedChar = '\0';
+    char str[100];
+    int stringLength;
+    if( uartUsb.readable() ) {
+        uartUsb.read( &receivedChar, 1 );
+        switch (receivedChar) {
+        case 'p':
+        case 'P':
+            potentiometerReading = potentiometer.read();
+            sprintf ( str, "Pot reading: %.3f\r\n", potentiometerReading );
+            stringLength = strlen(str);
+            uartUsb.write( str, stringLength );
+            break;
 
-   while (true) {
-      
-      if ( gasDetector || overTempDetector ) {
-         alarmLed = ON;
-      } else {
-         alarmLed = OFF;
-      }
-   }
+        case 'a':
+        case 'A': 
+            potentiometerReading = potentiometer.read();
+            sprintf (str, "Steering angle: %3f\r\n", potentiometerAngle(potentiometerReading));
+            stringLength = strlen(str);
+            uartUsb.write(str, stringLength);
+            break;
+        }
+    }
+}
+
+float potentiometerAngle(float potentiometerReading)
+{
+    return ( (potentiometerReading - 0.5) * 6 * 360);
 }
